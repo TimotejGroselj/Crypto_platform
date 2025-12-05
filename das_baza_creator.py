96a82a3169009acc0641f5522deba814850e6dae
Timotej Groselj <timotej.groselj1@gmail.com>
2025-12-05 10:48:33 +0100

    nimam pojma zakaj ne spremeni tabele coins

--- das_baza_creator.py
+++ das_baza_creator.py
@@ -3,20 +3,21 @@ from das_data import data
 connection = sql.connect("coin_data.db")
 cur = connection.cursor()
 query = """
-IF coins EXISTS 
-(CREATE TABLE coins (
-id_coin integer PRIMARY KEY AUTOINCREMENT,
+DROP TABLE IF EXISTS coins;
+CREATE TABLE coins (
+id_coin text PRIMARY KEY,
 name text NOT NULL
-));"""
-cur.execute(query)
+);"""
+cur.executescript(query)
 
 for coin in data:
     name = coin["name"]
+    id = coin["id"]
     query = f"""
             INSERT INTO coins
-            (name)
-            VALUES({name})
-            """
+            (id_coin,name)
+            VALUES('{id}','{name}')
+        """
     cur.execute(query)
 
 
