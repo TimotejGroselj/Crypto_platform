338468b48cca9d6ae7ba542fc9b6ca16967b1fab
Timotej Groselj <timotej.groselj1@gmail.com>
2025-12-05 15:31:41 +0100

    haha

--- das_baza_creator.py
+++ das_baza_creator.py
@@ -2,23 +2,25 @@ import sqlite3 as sql
 from das_data import data
 connection = sql.connect("coin_data.db")
 cur = connection.cursor()
-query = """
-DROP TABLE IF EXISTS coins;
-CREATE TABLE coins (
-id_coin text PRIMARY KEY,
-name text NOT NULL
-);"""
-cur.executescript(query)
+with connection:
+    query = """
+    DROP TABLE IF EXISTS coins;
+    CREATE TABLE coins (
+    id_coin text PRIMARY KEY,
+    name text NOT NULL
+    );"""
+    cur.executescript(query)
 
-for coin in data:
-    name = coin["name"]
-    id = coin["id"]
-    query = f"""
-            INSERT INTO coins
-            (id_coin,name)
-            VALUES('{id}','{name}')
-        """
-    cur.execute(query)
+    for coin in data:
+        name = coin["name"]
+        id = coin["id"]
+        query = f"""
+                INSERT INTO coins
+                (id_coin,name)
+                VALUES(?,?);
+                """
+        print(query)
+        cur.execute(query, (id,name))
 
 
 
--- das_data.py
+++ das_data.py
@@ -5,9 +5,6 @@ url="https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=price_
 response=requests.get(url)
 if response.status_code!=200:
     print("Call limit exceeded!")
-else:
-    print("Updating...")
-    print("This might take cca 5 min.")
 data=response.json()
 
 
