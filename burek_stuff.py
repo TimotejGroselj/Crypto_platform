d9ce937d1955d71e8eca803f9aec5e822f667757
Jauka <jaka.perbil@gmail.com>
2025-12-12 22:37:01 +0100

    pozen da dubis tabelo

--- burek_stuff.py
+++ burek_stuff.py
@@ -1,19 +1,34 @@
-import time
-from datetime import datetime
 import sqlite3 as sql
+from funkcije_encription import *
+import random
 
 
+conn = sql.connect("cryptodata.sqlite")
+cur = conn.cursor()
+q1 = """
+        DROP TABLE IF EXISTS assets;
+        CREATE TABLE assets (
+            wallet_id varchar(50),
+            coin_id varchar(15),
+            money numeric(15,6) NOT NULL,
+            PRIMARY KEY (wallet_id, coin_id)
+        );"""
 
-print("2025-12-12">'')
+qq = "SELECT user_id FROM users WHERE email = ?;"
+z = cur.execute(qq,['ecogle0@so-net.ne.jp']).fetchone()[0]
+print(z)
 
-print(datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d'))
+if not(0 < 100.5 < 100): print("yep")
 
-today = datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d')
-connection = sql.connect("cryptodata.sqlite")
-cur = connection.cursor()
-with connection:
-    q1 = "SELECT date FROM coins_prices WHERE date = ?;"
-    print(cur.execute(q1,['2025-12-09']).fetchall())
 
-if not [1]:
-    print('dad')
+with conn:
+    cur.executescript(q1)
+    querry = "SELECT user_id FROM users;"
+    le_tabelus = cur.execute(querry).fetchall()
+    coins = cur.execute("SELECT coin_id FROM coins;").fetchall()
+    for user_id in le_tabelus:
+        le_hash = id_to_hash(user_id[0])
+        cur.execute("INSERT INTO assets (wallet_id,coin_id,money) VALUES (?,?,?)", [le_hash,'EUR',round(random.uniform(200, 1000),2)])
+        for coin in coins:
+            q2 = "INSERT INTO assets (wallet_id, coin_id,money) VALUES (?,?,?)"
+            cur.execute(q2, [le_hash, coin[0],0])
