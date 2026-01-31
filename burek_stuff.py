8a78dca58dfe2ba5d206b27d42be4af80dcdf289
Jauka <jaka.perbil@gmail.com>
2026-01-31 21:15:23 +0100

    el butlus

--- burek_stuff.py
+++ burek_stuff.py
@@ -1,9 +1,30 @@
-import sqlite3 as sql
-from funkcije_encription import *
-import random
+from bottle import route, run, template, static_file, request,redirect
 
 
-conn = sql.connect("cryptodata.sqlite")
-cur = conn.cursor()
-qq = "SELECT username FROM users WHERE email = 'monk2@gmail.monk'"
-print(cur.execute(qq).fetchone()!=None)
+# 1. serve CSS
+@route('/static/<filename>')
+def static_files(filename):
+    return static_file(filename, root='./static')
+
+# 2. show login page
+@route('/')
+def show_login():
+    return template('login')
+
+@route('/login', method='POST')
+def login_logic():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+
+    if username == 'admin' and password == '1234':
+        return "<h1>Success!</h1>"
+    else:
+        return template('incorrect')
+
+
+@route('/dashboard')
+def dashboard():
+    return "<h1>You are logged in</h1>"
+
+# 3. start server
+run(host='localhost', port=8080, debug=True)
